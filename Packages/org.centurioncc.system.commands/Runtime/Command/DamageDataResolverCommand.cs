78edb26b6f6b7841bf9f39c4aa73694e2a2f3407
DerpyNewbie <derpynewbie@gmail.com>
2023-12-26 18:41:44 +0900

    Add DamageDataResolverCommand

---
+++ Packages/org.centurioncc.system.commands/Runtime/Command/DamageDataResolverCommand.cs
@@ -0,0 +1,162 @@
+﻿using CenturionCC.System.Player;
+using CenturionCC.System.Player.MassPlayer;
+using CenturionCC.System.Util;
+using DerpyNewbie.Common;
+using DerpyNewbie.Logger;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Data;
+using VRC.SDKBase;
+
+namespace CenturionCC.System.Command
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class DamageDataResolverCommand : NewbieConsoleCommandHandler
+    {
+        [SerializeField] [HideInInspector] [NewbieInject]
+        private PlayerManager playerManager;
+        [SerializeField] [HideInInspector] [NewbieInject]
+        private DamageDataSyncerManager syncerMgr;
+        [SerializeField] [HideInInspector] [NewbieInject]
+        private DamageDataResolver resolver;
+
+        [SerializeField]
+        private MockDamageData mockData;
+
+        public override string Label => "DamageDataResolver";
+        public override string[] Aliases => new[] { "DmgResolver", "DDResolver" };
+        public override string Description => "Configures DamageDataResolver";
+        public override string Usage =>
+            "<command> <getAssumedDiedTime|getConfirmedDiedTime|testInvokeHit> [value] OR <command> <printEvents|pause|continue|isPaused>";
+
+        public override string OnCommand(NewbieConsole console, string label, string[] vars, ref string[] envVars)
+        {
+            if (vars.Length == 0)
+                return console.PrintUsage(this);
+
+            switch (vars[0].ToLower())
+            {
+                // all strings are lowered
+                // ReSharper disable StringLiteralTypo
+                case "getassumed":
+                case "getassumedtime":
+                case "getassumeddiedtime":
+                {
+                    if (vars.Length <= 1)
+                    {
+                        console.Println("Requires Player ID value as argument");
+                        return ConsoleLiteral.GetNone();
+                    }
+
+                    var playerId = ConsoleParser.TryParseInt(vars[1]);
+                    if (playerId <= 0)
+                    {
+                        console.Println("Requires proper Player ID");
+                        return ConsoleLiteral.GetNone();
+                    }
+
+                    var assumedTime = resolver.GetAssumedDiedTime(playerId);
+                    return assumedTime.ToString("s");
+                }
+                case "getconfirmed":
+                case "getconfirmedtime":
+                case "getconfirmeddiedtime":
+                {
+                    if (vars.Length <= 1)
+                    {
+                        console.Println("Requires Player ID value as second argument");
+                        return ConsoleLiteral.GetNone();
+                    }
+
+                    var playerId = ConsoleParser.TryParseInt(vars[1]);
+                    if (playerId <= 0)
+                    {
+                        console.Println("Requires proper Player ID");
+                        return ConsoleLiteral.GetNone();
+                    }
+
+                    var player = playerManager.GetPlayerById(playerId);
+                    return player.LastHitData.HitTime.ToString("s");
+                }
+                case "ispaused":
+                {
+                    var paused = syncerMgr.ProcessingPaused;
+                    return ConsoleLiteral.Of(paused);
+                }
+                case "printevents":
+                {
+                    if (syncerMgr.GetEventsJson(JsonExportType.Beautify, out var jsonToken))
+                    {
+                        var jsonStr = jsonToken.String;
+                        console.Println(jsonStr);
+                        return jsonStr;
+                    }
+
+                    console.Println(jsonToken.Error.ToString());
+                    return ConsoleLiteral.GetNone();
+                }
+                case "pause":
+                {
+                    console.Println("Requested SyncerManager to pause processing");
+                    syncerMgr.PauseProcessing();
+                    return ConsoleLiteral.GetNone();
+                }
+                case "continue":
+                {
+                    console.Println("Requested SyncerManager to continue processing");
+                    syncerMgr.ContinueProcessing();
+                    return ConsoleLiteral.GetNone();
+                }
+                case "testinvokehit":
+                {
+                    if (vars.Length <= 1)
+                    {
+                        console.Println("Requires Player ID value as second argument");
+                        return ConsoleLiteral.GetNone();
+                    }
+
+                    var playerId = ConsoleParser.TryParseInt(vars[1]);
+                    if (playerId <= 0)
+                    {
+                        console.Println("Requires proper Player ID");
+                        return ConsoleLiteral.GetNone();
+                    }
+
+                    var model = (PlayerModel)playerManager.GetPlayerById(playerId);
+                    if (model == null)
+                    {
+                        console.Println("Requires in-game Player ID");
+                        return ConsoleLiteral.GetNone();
+                    }
+
+                    if (model.playerView == null)
+                    {
+                        console.Println("Requires player model with view");
+                        return ConsoleLiteral.GetNone();
+                    }
+
+                    var view = model.playerView;
+                    var pCol = view.GetColliders()[0];
+
+                    mockData.SetData(
+                        true,
+                        Networking.LocalPlayer.playerId,
+                        Networking.LocalPlayer.GetPosition(),
+                        Networking.LocalPlayer.GetRotation(),
+                        Networking.GetNetworkDateTime(),
+                        "TestInvokeHit"
+                    );
+
+                    console.Println($"Requested resolve to player {NewbieUtils.GetPlayerName(model.VrcPlayer)}");
+                    syncerMgr.OnHitDetection(pCol, mockData, model.Position);
+                    return ConsoleLiteral.GetNone();
+                }
+                default:
+                {
+                    return console.PrintUsage(this);
+                }
+                // ReSharper restore StringLiteralTypo
+            }
+        }
+    }
+}
---
+++ Packages/org.centurioncc.system.commands/Runtime/Util/MockDamageData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using CenturionCC.System.Utils;
+using UdonSharp;
+using UnityEngine;
+
+namespace CenturionCC.System.Util
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class MockDamageData : DamageData
+    {
+        private Vector3 _damageOriginPos;
+        private Quaternion _damageOriginRot;
+        private DateTime _damageOriginTime;
+        private int _damagerPlayerId;
+        private string _damageType;
+        private bool _shouldApplyDamage;
+
+        public override bool ShouldApplyDamage => _shouldApplyDamage;
+        public override int DamagerPlayerId => _damagerPlayerId;
+        public override Vector3 DamageOriginPosition => _damageOriginPos;
+        public override Quaternion DamageOriginRotation => _damageOriginRot;
+        public override DateTime DamageOriginTime => _damageOriginTime;
+        public override string DamageType => _damageType;
+
+        public void SetData(bool apply, int playerId, Vector3 pos, Quaternion rot, DateTime time, string type)
+        {
+            _shouldApplyDamage = apply;
+            _damagerPlayerId = playerId;
+            _damageOriginPos = pos;
+            _damageOriginRot = rot;
+            _damageOriginTime = time;
+            _damageType = type;
+        }
+    }
+}
