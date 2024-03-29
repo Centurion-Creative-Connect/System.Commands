ae709e40aa35a9c58b0a98a35bd690565d4fb855
DerpyNewbie <derpynewbie@gmail.com>
2024-03-29 17:19:57 +0900

    Fix late-joiner desync for StaffOnlyToggleObjectCommand

--- Packages/org.centurioncc.system.commands/Runtime/Command/StaffOnlyToggleObjectCommand.cs
+++ Packages/org.centurioncc.system.commands/Runtime/Command/StaffOnlyToggleObjectCommand.cs
@@ -1,42 +1,41 @@
-﻿using DerpyNewbie.Logger;
+﻿using DerpyNewbie.Common;
+using DerpyNewbie.Logger;
 using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
+using VRC.Udon.Common.Interfaces;
 
 namespace CenturionCC.System.Command
 {
     [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
     public class StaffOnlyToggleObjectCommand : BoolCommandHandler
     {
-        [SerializeField]
-        private bool defaultState = true;
-        [Header("Global")]
-        [SerializeField]
-        private GameObject[] globalObjectsToEnable;
-        [SerializeField]
-        private GameObject[] globalObjectsToDisable;
-        [Header("Moderator")]
-        [SerializeField]
-        private GameObject[] moderatorOnlyObjectsToEnable;
-        [SerializeField]
-        private GameObject[] moderatorOnlyObjectsToDisable;
-        [SerializeField]
-        private GameObject[] moderatorOnlyObjectsToAlwaysDisable;
-        [SerializeField]
-        private GameObject[] moderatorOnlyObjectsToAlwaysEnable;
-        [Header("Player")]
-        [SerializeField]
-        private GameObject[] playerOnlyObjectsToEnable;
-        [SerializeField]
-        private GameObject[] playerOnlyObjectsToDisable;
-        [SerializeField]
-        private GameObject[] playerOnlyObjectsToAlwaysDisable;
-        [SerializeField]
-        private GameObject[] playerOnlyObjectsToAlwaysEnable;
-        [SerializeField]
-        private Collider[] playerOnlyCollidersToDisable;
+        [SerializeField] private bool defaultState = true;
+
+        [Header("Global")] [SerializeField] private GameObject[] globalObjectsToEnable;
+
+        [SerializeField] private GameObject[] globalObjectsToDisable;
+
+        [Header("Moderator")] [SerializeField] private GameObject[] moderatorOnlyObjectsToEnable;
+
+        [SerializeField] private GameObject[] moderatorOnlyObjectsToDisable;
+
+        [SerializeField] private GameObject[] moderatorOnlyObjectsToAlwaysDisable;
+
+        [SerializeField] private GameObject[] moderatorOnlyObjectsToAlwaysEnable;
+
+        [Header("Player")] [SerializeField] private GameObject[] playerOnlyObjectsToEnable;
+
+        [SerializeField] private GameObject[] playerOnlyObjectsToDisable;
+
+        [SerializeField] private GameObject[] playerOnlyObjectsToAlwaysDisable;
+
+        [SerializeField] private GameObject[] playerOnlyObjectsToAlwaysEnable;
+
+        [SerializeField] private Collider[] playerOnlyCollidersToDisable;
 
         private NewbieConsole _console;
+
         [UdonSynced, FieldChangeCallback(nameof(CurrentState))]
         private bool _currentState;
 
@@ -46,7 +45,7 @@ namespace CenturionCC.System.Command
             set
             {
                 _currentState = value;
-                UpdateObjectsState();
+                UpdateObjectsState(value);
             }
         }
 
@@ -55,13 +54,19 @@ namespace CenturionCC.System.Command
 
         private void Start()
         {
+            SendCustomEventDelayedFrames(nameof(_LateStart), 1);
+        }
+
+        public void _LateStart()
+        {
             if (Networking.IsMaster)
             {
                 CurrentState = defaultState;
                 RequestSerialization();
             }
 
-            UpdateObjectsState();
+            UpdateObjectsState(CurrentState);
+            RequestSerialization();
         }
 
         private void SetActiveAll(GameObject[] objs, bool isActive)
@@ -72,28 +77,38 @@ namespace CenturionCC.System.Command
                     o.SetActive(isActive);
         }
 
-        private void UpdateObjectsState()
+        private void UpdateObjectsState(bool state)
         {
-            SetActiveAll(globalObjectsToDisable, !CurrentState);
-            SetActiveAll(globalObjectsToEnable, CurrentState);
+            SetActiveAll(globalObjectsToDisable, !state);
+            SetActiveAll(globalObjectsToEnable, state);
 
             if (_console != null && _console.IsSuperUser)
             {
-                SetActiveAll(moderatorOnlyObjectsToDisable, !CurrentState);
-                SetActiveAll(moderatorOnlyObjectsToEnable, CurrentState);
+                SetActiveAll(moderatorOnlyObjectsToDisable, !state);
+                SetActiveAll(moderatorOnlyObjectsToEnable, state);
                 SetActiveAll(moderatorOnlyObjectsToAlwaysDisable, false);
                 SetActiveAll(moderatorOnlyObjectsToAlwaysEnable, true);
             }
             else
             {
-                SetActiveAll(playerOnlyObjectsToDisable, !CurrentState);
-                SetActiveAll(playerOnlyObjectsToEnable, CurrentState);
+                SetActiveAll(playerOnlyObjectsToDisable, !state);
+                SetActiveAll(playerOnlyObjectsToEnable, state);
                 SetActiveAll(playerOnlyObjectsToAlwaysDisable, false);
                 SetActiveAll(playerOnlyObjectsToAlwaysEnable, true);
                 foreach (var c in playerOnlyCollidersToDisable)
                     if (c != null)
-                        c.enabled = !CurrentState;
+                        c.enabled = !state;
             }
+        }
+
+        public void ForceSetActiveTrue()
+        {
+            CurrentState = true;
+        }
+
+        public void ForceSetActiveFalse()
+        {
+            CurrentState = false;
         }
 
         public override bool OnBoolCommand(NewbieConsole console, string label, ref string[] vars, ref string[] envVars)
@@ -104,12 +119,25 @@ namespace CenturionCC.System.Command
                 return CurrentState;
             }
 
+            var force = false;
+            if (vars.ContainsString("-f"))
+            {
+                force = true;
+                vars = vars.RemoveItem("-f");
+            }
+
             if (vars.Length >= 1)
             {
                 var result = ConsoleParser.TryParseBoolean(vars[0], CurrentState);
                 CurrentState = result;
                 Networking.SetOwner(Networking.LocalPlayer, gameObject);
                 RequestSerialization();
+
+                if (force)
+                {
+                    SendCustomNetworkEvent(NetworkEventTarget.All,
+                        result ? nameof(ForceSetActiveTrue) : nameof(ForceSetActiveFalse));
+                }
             }
 
             console.Println($"{label}: {CurrentState}");
@@ -119,7 +147,6 @@ namespace CenturionCC.System.Command
         public override void OnRegistered(NewbieConsole console)
         {
             _console = console;
-            UpdateObjectsState();
         }
     }
 }
